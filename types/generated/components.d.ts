bd717747c31ffc297b8805a034454517a8df36e1
elsline <am2505096@gmail.com>
2024-08-05 08:18:57 +0300

    s

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -4,12 +4,18 @@ export interface ChaptersChapter extends Schema.Component {
   collectionName: 'components_chapters_chapters';
   info: {
     displayName: 'chapter';
+    description: '';
   };
   attributes: {
     chapter_name: Attribute.String;
     chapter_number: Attribute.Integer;
     colored: Attribute.Boolean;
     pages: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+    manga_lists: Attribute.Relation<
+      'chapters.chapter',
+      'oneToMany',
+      'api::manga-list.manga-list'
+    >;
   };
 }
 
